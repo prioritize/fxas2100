151e3fc173541095e474bbec5e8a8dd94cd5fab5
prioritize <jared@prototypic.al>
2025-04-22 15:23:32 -0700

    progress

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,19 @@
 #![no_std]
+pub mod commands;
 pub mod odr;
+pub mod outputs;
 pub mod registers;
 use crate::registers::*;
 use defmt::println;
 use embassy_sync::blocking_mutex::raw::{CriticalSectionRawMutex, NoopRawMutex};
 use embassy_sync::signal::Signal;
 use odr::DataRate;
+
+pub enum State {
+    Active,
+    Ready,
+    Standby
+}
 
 pub enum Error<I2cError> {
     I2C(I2cError),
@@ -15,6 +23,7 @@ pub struct FXAS2100<I2C> {
     pub i2c: I2C,
     pub address: u8,
     pub collect_signal: &'static Signal<CriticalSectionRawMutex, bool>,
+    pub state: State,
 }
 
 // impl<I2C, E> embedded_hal_async::i2c::ErrorType for FXAS2100<I2C>
@@ -50,20 +59,25 @@ where
             collect_signal,
         }
     }
-    pub fn status() {}
+    pub async fn status(&mut self) -> u8 {
+        let mut data = [0u8; 1];
+        self.read_bytes(Registers::STATUS.to_u8(), &mut data).await;
+        data[0]
+    }
     pub async fn who_am_i(&mut self) -> u8 {
-        self.read_register(registers::WHO_AM_I).await
+        self.read_register(Registers::WHO_AM_I.to_u8()).await
     }
     pub async fn set_register(&mut self, register: u8, value: u8) {
         let _ = self.i2c.write(self.address, &[register, value]).await;
     }
+
     pub fn set_output_data_rate() {}
     pub fn read_byte() {}
     pub async fn set_odr(&mut self, rate: DataRate) {
         let mut register_state = 0u8;
         println!("odr_register: {}", register_state);
         let _ = self
-            .read_bytes(registers::CTRL_REG1, &mut [register_state])
+            .read_bytes(Registers::CTRL_REG1.to_u8(), &mut [register_state])
             .await;
         register_state |= odr::ODR_MASK;
         println!("odr_register: {}", register_state);
@@ -88,14 +102,11 @@ where
     }
     pub fn read_temp() {}
     async fn set_active(&mut self) {
-        let current_state = self.read_register(crate::registers::CTRL_REG1).await;
+        let current_state = self.read_register(Registers::CTRL_REG1.to_u8()).await;
         let _ = self
-            .i2c
-            .write(self.address, &[
-                registers::CTRL_REG1,
-                current_state | 0b00000010,
-            ])
+            .set_register(Registers::CTRL_REG1.to_u8(), current_state | 0b00000010)
             .await;
+        self.
     }
     pub async fn signal_active(&self) {
         self.collect_signal.signal(true);
--- src/registers.rs
+++ src/registers.rs
@@ -1,33 +1,33 @@
-pub const STATUS: u8 = 0x00;
-pub const OUT_X_MSB: u8 = 0x01;
-pub const OUT_X_LSB: u8 = 0x02;
-pub const OUT_Y_MSB: u8 = 0x03;
-pub const OUT_Y_LSB: u8 = 0x04;
-pub const OUT_Z_MSB: u8 = 0x05;
-pub const OUT_Z_LSB: u8 = 0x06;
-pub const DR_STATUS: u8 = 0x07;
-pub const F_STATUS: u8 = 0x08;
-pub const F_SETUP: u8 = 0x09;
-pub const F_EVENT: u8 = 0x0A;
-pub const INT_SRC_FLAG: u8 = 0x0B;
-pub const WHO_AM_I: u8 = 0x0C;
-pub const CTRL_REG0: u8 = 0x0D;
-pub const RT_CFG: u8 = 0x0E;
-pub const RT_SRC: u8 = 0x0F;
-pub const RT_THS: u8 = 0x10;
-pub const RT_COUNT: u8 = 0x11;
-pub const TEMP: u8 = 0x12;
-pub const CTRL_REG1: u8 = 0x13;
-pub const CTRL_REG2: u8 = 0x14;
-pub const CTRL_REG3: u8 = 0x15;
-pub const ODR_RATE_800: u8 = 0;
-pub const ODR_RATE_400: u8 = 1;
-pub const ODR_RATE_200: u8 = 2;
-pub const ODR_RATE_100: u8 = 3;
-pub const ODR_RATE_50: u8 = 4;
-pub const ODR_RATE_25: u8 = 5;
-pub const ODR_RATE_12HALF: u8 = 6;
-pub const ODR_RATE_12HALF_1: u8 = 7;
+// pub const STATUS: u8 = 0x00;
+// pub const OUT_X_MSB: u8 = 0x01;
+// pub const OUT_X_LSB: u8 = 0x02;
+// pub const OUT_Y_MSB: u8 = 0x03;
+// pub const OUT_Y_LSB: u8 = 0x04;
+// pub const OUT_Z_MSB: u8 = 0x05;
+// pub const OUT_Z_LSB: u8 = 0x06;
+// pub const DR_STATUS: u8 = 0x07;
+// pub const F_STATUS: u8 = 0x08;
+// pub const F_SETUP: u8 = 0x09;
+// pub const F_EVENT: u8 = 0x0A;
+// pub const INT_SRC_FLAG: u8 = 0x0B;
+// pub const WHO_AM_I: u8 = 0x0C;
+// pub const CTRL_REG0: u8 = 0x0D;
+// pub const RT_CFG: u8 = 0x0E;
+// pub const RT_SRC: u8 = 0x0F;
+// pub const RT_THS: u8 = 0x10;
+// pub const RT_COUNT: u8 = 0x11;
+// pub const TEMP: u8 = 0x12;
+// pub const CTRL_REG1: u8 = 0x13;
+// pub const CTRL_REG2: u8 = 0x14;
+// pub const CTRL_REG3: u8 = 0x15;
+// pub const ODR_RATE_800: u8 = 0;
+// pub const ODR_RATE_400: u8 = 1;
+// pub const ODR_RATE_200: u8 = 2;
+// pub const ODR_RATE_100: u8 = 3;
+// pub const ODR_RATE_50: u8 = 4;
+// pub const ODR_RATE_25: u8 = 5;
+// pub const ODR_RATE_12HALF: u8 = 6;
+// pub const ODR_RATE_12HALF_1: u8 = 7;
 
 pub enum Registers {
     STATUS,
@@ -52,4 +52,32 @@ pub enum Registers {
     CTRL_REG1,
     CTRL_REG2,
     CTRL_REG3,
+}
+impl Registers {
+    pub const fn to_u8(&self) -> u8 {
+        match self {
+            Registers::STATUS => 0x00,
+            Registers::OUT_X_MSB => 0x01,
+            Registers::OUT_X_LSB => 0x02,
+            Registers::OUT_Y_MSB => 0x03,
+            Registers::OUT_Y_LSB => 0x04,
+            Registers::OUT_Z_MSB => 0x05,
+            Registers::OUT_Z_LSB => 0x06,
+            Registers::DR_STATUS => 0x07,
+            Registers::F_STATUS => 0x08,
+            Registers::F_SETUP => 0x09,
+            Registers::F_EVENT => 0x0A,
+            Registers::INT_SRC_FLAG => 0x0B,
+            Registers::WHO_AM_I => 0x0C,
+            Registers::CTRL_REG0 => 0x0D,
+            Registers::RT_CFG => 0x0E,
+            Registers::RT_SRC => 0x0F,
+            Registers::RT_THS => 0x10,
+            Registers::RT_COUNT => 0x11,
+            Registers::TEMP => 0x12,
+            Registers::CTRL_REG1 => 0x13,
+            Registers::CTRL_REG2 => 0x14,
+            Registers::CTRL_REG3 => 0x15,
+        }
+    }
 }
