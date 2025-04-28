c7fbe133f4a902a9bee71683de54d38f2dc0e5c6
prioritize <jared@prototypic.al>
2025-04-28 16:12:59 -0700

    progress

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,25 @@
 #![no_std]
 pub mod commands;
+pub mod fifo;
+pub mod masks;
 pub mod odr;
 pub mod outputs;
 pub mod registers;
 use crate::registers::Registers::*;
 use defmt::println;
-use embassy_sync::blocking_mutex::raw::{CriticalSectionRawMutex, NoopRawMutex};
-use embassy_sync::signal::Signal;
-use embassy_time::{Duration, Timer};
+use embassy_time::Timer;
+use fifo::Mode;
+use masks::Masks;
 use odr::{DataRate, ODR_MASK};
 
+pub const DEFAULT_ADDRESS: u8 = 0x21;
+
+#[derive(Debug, Clone)]
+pub enum FXASError {
+    ResetError,
+    FifoConfigureError,
+}
+type Result<T> = core::result::Result<T, FXASError>;
 #[derive(PartialEq)]
 pub enum State {
     Active,
@@ -53,12 +63,37 @@ where
             state: State::Standby,
             data_rate: DataRate::EightHundred,
         };
-        let mut ctrl_reg = [0u8; 1];
-        fxas.set_register(CTRL_REG1.to_u8(), 0b01000000).await;
-        Timer::after_millis(50).await;
-        fxas.read_bytes(CTRL_REG1.to_u8(), &mut ctrl_reg).await;
-        assert_eq!(ctrl_reg[0], 0);
+        let _ = fxas.reset().await;
         fxas
+    }
+    pub async fn reset(&mut self) -> Result<()> {
+        let mut data = [0u8; 1];
+        self.set_register(CtrlReg1.to_u8(), Masks::Reset.to_mask())
+            .await;
+        self.read_bytes(CtrlReg1.to_u8(), &mut data).await;
+        Timer::after_millis(50).await;
+        match 0x00 == data[0] {
+            true => Ok(()),
+            false => {
+                println!("FXAS21002 failed to reset correctly");
+                Err(FXASError::ResetError)
+            }
+        }
+    }
+    pub async fn configure_fifo_mode(&mut self, mode: Mode) -> Result<()> {
+        let mut f_setup = [0u8; 1];
+        self.set_standby().await;
+        self.read_bytes(FSetup.to_u8(), &mut f_setup).await;
+        self.set_register(FSetup.to_u8(), toggle_on(f_setup[0], mode.to_u8()))
+            .await;
+        self.read_bytes(FSetup.to_u8(), &mut f_setup).await;
+        f_setup[0] &= mode.to_u8();
+
+        if f_setup[0] != mode.to_u8() {
+            Err(FXASError::FifoConfigureError)
+        } else {
+            Ok(())
+        }
     }
     pub async fn status(&mut self) -> u8 {
         let mut data = [0u8; 1];
@@ -66,7 +101,7 @@ where
         data[0]
     }
     pub async fn who_am_i(&mut self) -> u8 {
-        self.read_register(WHO_AM_I.to_u8()).await
+        self.read_register(WhoAmI.to_u8()).await
     }
     pub async fn set_register(&mut self, register: u8, value: u8) {
         let _ = self.i2c.write(self.address, &[register, value]).await;
@@ -76,11 +111,11 @@ where
         let mut register_state = 0u8;
         println!("odr_register: {}", register_state);
         let _ = self
-            .read_bytes(CTRL_REG1.to_u8(), &mut [register_state])
+            .read_bytes(CtrlReg1.to_u8(), &mut [register_state])
             .await;
         println!("current odr setting: {}", register_state & ODR_MASK);
         register_state = toggle_on(toggle_off(!ODR_MASK, register_state), ack_rate);
-        self.set_register(CTRL_REG1.to_u8(), toggle_on(ODR_MASK, register_state))
+        self.set_register(CtrlReg1.to_u8(), toggle_on(ODR_MASK, register_state))
             .await;
         self.data_rate = rate;
     }
@@ -95,43 +130,53 @@ where
             .await
         {
             Ok(b) => println!("{}", b),
-            Err(e) => println!("error"),
+            Err(_) => println!("error"),
         }
 
         defmt::println!("{}", data);
         data[0]
     }
-    pub fn read_temp() {}
+    pub async fn read_temp(&mut self) -> Result<u8> {
+        let mut temp = [0u8; 1];
+        self.read_bytes(Temp.to_u8(), &mut temp).await;
+        Ok(temp[0])
+    }
     pub async fn set_active(&mut self) {
-        let current_state = self.read_register(CTRL_REG1.to_u8()).await;
+        let current_state = self.read_register(CtrlReg1.to_u8()).await;
         let _ = self
-            .set_register(CTRL_REG1.to_u8(), toggle_on(0x02, current_state))
+            .set_register(CtrlReg1.to_u8(), toggle_on(0x02, current_state))
             .await;
         self.state = State::Active;
     }
     pub async fn set_standby(&mut self) {
-        let current_state = self.read_register(CTRL_REG1.to_u8()).await;
+        let current_state = self.read_register(CtrlReg1.to_u8()).await;
         let _ = self
-            .set_register(CTRL_REG1.to_u8(), toggle_off(0x03, current_state))
+            .set_register(CtrlReg1.to_u8(), toggle_off(0x03, current_state))
             .await;
         self.state = State::Standby;
     }
     pub async fn set_ready(&mut self) {
-        let current_state = self.read_register(CTRL_REG1.to_u8()).await;
+        let current_state = self.read_register(CtrlReg1.to_u8()).await;
         let _ = self
-            .set_register(CTRL_REG1.to_u8(), toggle_on(0x01, current_state))
+            .set_register(CtrlReg1.to_u8(), toggle_on(0x01, current_state))
             .await;
         self.state = State::Ready;
     }
     pub async fn collect_gyro_data(&mut self) -> [u8; 6] {
         let mut data = [0u8; 6];
-        self.read_bytes(OUT_X_MSB.to_u8(), &mut data).await;
+        self.read_bytes(OutXMsb.to_u8(), &mut data).await;
         data
+    }
+    pub async fn get_fifo_count(&mut self) -> u8 {
+        let mut sample_count = 0u8;
+        self.read_bytes(FStatus.to_u8(), &mut [sample_count]).await;
+        sample_count &= Masks::FifoCount.to_mask();
+        sample_count
     }
     pub async fn enable_self_test(&mut self) {
         let mut reg_state = [0u8; 1];
-        self.read_bytes(CTRL_REG1.to_u8(), &mut reg_state).await;
-        self.set_register(CTRL_REG1.to_u8(), toggle_on(0x20, reg_state[0]))
+        self.read_bytes(CtrlReg1.to_u8(), &mut reg_state).await;
+        self.set_register(CtrlReg1.to_u8(), toggle_on(0x20, reg_state[0]))
             .await;
     }
 }
@@ -148,7 +193,7 @@ where
         }
     }
     pub fn blocking_read_register(&mut self, register: u8) -> u8 {
-        let mut data = 0x0;
+        let data = 0x0;
         let _ = self.i2c.write_read(self.address, &[register], &mut [data]);
         data
     }
